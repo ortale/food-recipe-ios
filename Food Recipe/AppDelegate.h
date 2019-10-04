285ddbe23c3151cbddf13be9347df6cf436f8dc3
Jose Ortale <jose.ortale@pzbp.co.uk>
2019-10-04 08:51:58 +0100

    First commit

---
+++ Food Recipe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Food Recipe
+//
+//  Created by Jose Ortale on 04/10/2019.
+//  Copyright © 2019 Jose Ortale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Food Recipe/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Food Recipe
+//
+//  Created by Jose Ortale on 04/10/2019.
+//  Copyright © 2019 Jose Ortale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
